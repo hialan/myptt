202c5b7914d5738adab227b2f068a14afacb7005
Hialan Liu <hialan.liu@gmail.com>
2016-01-30 19:03:19 +0800

    add Client and ansi_parser

--- ptt.php
+++ ptt.php
@@ -1,103 +1,40 @@
 <?php
-require_once 'Telnet.class.php';
-require_once 'ByteStream.inc';
-require_once 'Term.inc';
+require_once __DIR__ . '/Client.inc';
 
 $config = parse_ini_file("config.ini", true);
 $config = $config['global'];
 
-class Processer {
-    private $term;
 
-    public function __construct() {
-        $this->term = new TermBuf(80, 24);
-    }
+$client = new Client('ptt.cc', 23);
+$client->exec('', false);
+echo $client->getScreen();
 
-    public function process_input($buf) {
-        $byteStream = new ByteStream($buf);
-        $result = '';	
-        $state = 'text';
-        while(!$byteStream->eof()) {
-            $c = $byteStream->getc();
-            if($state == 'text') {
-                if($c->isEsc()) {
-                    $state = 'esc';
-                    $result .= $c->getChar();
-                } else if ($c->isBig5FirstByte()){
-                    $state = 'chinese';
-                    $ch1 = $c;
-                } else {
-                    $result .= $c->getChar();
-                }
-            } else if($state == 'chinese') {
-                $control = '';
-                if($c->isEsc()) {
-                    while($c->getChar() != 'm') {
-                        $control .= $c->getChar();
-                        $c = $byteStream->getc();
-                    }
-                    $control .= $c->getChar();
-                    $c = $byteStream->getc();
-                }
-                $state = 'text';
-                $ch2 = $c;
-                $utf8 = $ch1->toChinese($ch2->getByte());
-                $result .= $utf8;
-                if(!empty($control)) {
-                    $result .= $control;
-                }
-            } else if($state == 'esc') {
-                $b = $c->getByte();
-                if($b == ord('m') || $b == ord('K') || $b == ord('H')) {
-                    $state = 'text';
-                }
-                //error_log(chr($c), 3, './log.txt');
-                $result .= $c->getChar();
-            }
-        }
-        return $result;
-    }
-}
+$client->exec($config['user']);
+echo $client->getScreen();
 
-$proc = new Processer();
+$client->exec($config['password']);
+echo $client->getScreen();
 
-$client = new Telnet('ptt.cc', 23, 10, '');
-$data = $client->exec('', false);
-echo $proc->process_input($data);
-
-$data = $client->exec($config['user']);
-echo $proc->process_input($data);
-
-$data .= $client->exec($config['password']);
-echo $proc->process_input($data);
-
-
-$data = $client->exec('');
-echo $proc->process_input($data);
+$client->exec('');
+echo $client->getScreen();
 sleep(1);
 
-//return;
-
-/*
-$data = $client->exec('n');
-echo process_input($data);
-sleep(1);
-*/
-
-$data = $client->exec('sGossiping');
-$screen = $proc->process_input($data);
+$client->exec('sGossiping');
+$screen = $client->getScreen();
 echo $screen;
 sleep(1);
 
+
 if(mb_strpos($screen, '請按任意鍵繼續')) {
-	$data = $client->exec('');
-	$screen = $proc->process_input($data);
+	$client->exec('');
+	$screen = $client->getScreen();
 	echo $screen;
 	sleep(1);
 }
 
-$data = $client->exec('Z100');
-echo $proc->process_input($data);
+
+$client->exec('Z100');
+echo $client->getScreen();
 sleep(1);
 
 $client->disconnect();
